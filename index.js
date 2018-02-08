2762223506fec6d632f227165299d4bb2c29472a
Brandon Cassidy <brandoncassidy11@yahoo.com>
2018-02-08 03:30:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
+  var priceTotal = total();
   if (!cardNumber) {
     console.log('Sorry, we don\'t have a credit card on file for you.');
   }
